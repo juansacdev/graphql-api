e8fcd969c836690486e79ccaf4b26d3aecb4842d
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-02-12 03:10:34 -0500

    Creamos el type personalizado Course

--- src/lib/resolvers.js
+++ src/lib/resolvers.js
@@ -1,10 +1,31 @@
 // Configuramos los resolvers
 
-module.exports = {
-    hello: () => {
-        return `Hola Mundo!`
+const courses = [
+    {
+        _id: '1',
+        title: '1 Title',
+        teacher: '1 Teacher',
+        description: '1 Description',
+        topic: '1 Topic',
     },
-    saludo: () => {
-        return 'Hola a todos'
+    {
+        _id: '2',
+        title: '2 Title',
+        teacher: '2 Teacher',
+        description: '2 Description',
+        topic: '2 Topic',
+    },
+    {
+        _id: '3',
+        title: '3 Title',
+        teacher: '3 Teacher',
+        description: '3 Description',
+        topic: '3 Topic',
     }
+]
+
+module.exports = {
+    getCourses: () => {
+        return courses
+    },
 }
