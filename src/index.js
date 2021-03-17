0ddd7cb0fae5217ac35b13f9c1fc68dcdd8f8c39
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-03-16 21:44:59 -0500

    restructures project for graphiql on web

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const { graphqlHTTP } = require('express-graphql')
+const { buildSchema } = require('graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const { PORT } = require('../setting')
+const graphQLResolvers = require('./lib/resolvers')
+const express = require('express')
+const app = express()
+
+// Reading GraphQL Schema
+const grapqhQLSchema = buildSchema(
+    readFileSync(
+        join(__dirname, 'lib', 'schema.graphql'),
+        'utf-8'
+    )
+)
+
+// Inicialization & Middlewares
+app.use('/', graphqlHTTP({
+    schema: grapqhQLSchema,
+    rootValue: graphQLResolvers,
+    graphiql: true,
+}))
+
+//  Listening
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
---
+++ src/lib/resolvers.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    hello: () => {
+        return 'Hola mundo con GraphQL'
+    },
+
+    saludo: () => {
+        return 'Hola a todos desde GraphQL'
+    }
+}
